76c490aad5c2bc6108acf95f5f9ac41935a6f7ee
Кирилл Попов <diplomatgmg@gmail.com>
2023-10-07 17:04:01 +0300

    reformat code

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,30 +1,30 @@
 #!/usr/bin/env node
 
-import readlineSync from "readline-sync";
-import getName from "../src/cli.js";
-import printWelcome from "./brain-games.js";
+import readlineSync from 'readline-sync';
+import getName from '../src/cli.js';
+import printWelcome from './brain-games.js';
 
 function evenOrOddGame() {
-    console.log("Answer \"yes\" if the number is even, otherwise answer \"no\".");
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
-    for (let i = 0; i < 3; i += 1) {
-        const randomNumber = Math.floor(Math.random() * 100);
+  for (let i = 0; i < 3; i += 1) {
+    const randomNumber = Math.floor(Math.random() * 100);
 
-        console.log(`Question ${randomNumber}`);
+    console.log(`Question ${randomNumber}`);
 
-        const userAnswer = readlineSync.question("Your answer: ");
-        const isEven = randomNumber % 2 === 0;
-        const isCorrect = (userAnswer === "yes" && isEven) || (userAnswer === "no" && !isEven);
+    const userAnswer = readlineSync.question('Your answer: ');
+    const isEven = randomNumber % 2 === 0;
+    const isCorrect = (userAnswer === 'yes' && isEven) || (userAnswer === 'no' && !isEven);
 
-        if (isCorrect) {
-            console.log("Correct!");
-        } else {
-            const correctAnswer = userAnswer === "yes" ? "no" : "yes";
-            console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer}".`);
-            return false;
-        }
+    if (isCorrect) {
+      console.log('Correct!');
+    } else {
+      const correctAnswer = userAnswer === 'yes' ? 'no' : 'yes';
+      console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer}".`);
+      return false;
     }
-    return true;
+  }
+  return true;
 }
 
 const name = getName();
@@ -33,7 +33,7 @@ printWelcome(name);
 const winGame = evenOrOddGame();
 
 if (winGame) {
-    console.log(`Congratulations, ${name}!`);
+  console.log(`Congratulations, ${name}!`);
 } else {
-    console.log(`Try one more time, ${name} <3`);
+  console.log(`Try one more time, ${name} <3`);
 }
--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
 export default function printWelcome(name) {
-    console.log("Welcome to the Brain Games!");
-    console.log(`Hello, ${name}!`);
+  console.log('Welcome to the Brain Games!');
+  console.log(`Hello, ${name}!`);
 }
