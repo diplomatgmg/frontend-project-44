929425aa30b6a96134e55fdfc03dd5a50eea0352
Кирилл Попов <diplomatgmg@gmail.com>
2023-10-07 15:04:55 +0300

    update mini-game logic

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,9 +1,8 @@
 #!/usr/bin/env node
+
 import getName from "../src/cli.js";
-import EvenOrOddGame from "./brain-even.js";
 
 console.log("Welcome to the Brain Games!");
 
 const name = getName();
-EvenOrOddGame();
-console.log(`Congratulations, ${name}!`);
+console.log(`Hello, ${name}!`);
