abee2423666fdd24ba6f856cdaf01f1e782307d0
Кирилл Попов <diplomatgmg@gmail.com>
2023-10-08 19:38:06 +0300

    fix bugs in function getGreatestDivisor

--- services/brain-gcd-service.js
+++ services/brain-gcd-service.js
@@ -1,9 +1,10 @@
 function getGreatestDivisor(num1, num2) {
   const minNumber = Math.min(num1, num2);
-  let maxGreatestDivisor;
+  let maxGreatestDivisor = 1;
   for (let i = minNumber; i > 0; i -= 1) {
     if (num1 % i === 0 && num2 % i === 0) {
       maxGreatestDivisor = i;
+      break;
     }
   }
   return maxGreatestDivisor;
