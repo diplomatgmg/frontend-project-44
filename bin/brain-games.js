03f2a3a976691d8c7db6451613925357d1e499d8
Кирилл Попов <diplomatgmg@gmail.com>
2023-10-08 18:50:36 +0300

    fix file brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,11 @@
 #!/usr/bin/env node
 
-export default function printWelcome() {
+import getUserName from '../src/cli.js';
+
+function printWelcome() {
   console.log('Welcome to the Brain Games!');
+  const name = getUserName();
+  console.log(`Hello, ${name}!`);
 }
+
+printWelcome();
