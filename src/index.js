480347ff12d06a8f1aaf5b78ccfaafc7ed4dcb15
Кирилл Попов <diplomatgmg@gmail.com>
2023-12-15 13:00:29 +0300

    Фиксы

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import readlineSync from 'readline-sync';
+import getName from './cli.js';
 
 export default function runGame(gameFunction, gameInstructionMessage) {
   console.log('Welcome to the Brain Games!');
 
-  const name = readlineSync.question('May I have your name? ');
+  const name = getName();
   console.log(`Hello, ${name}!`);
 
   console.log(gameInstructionMessage);
